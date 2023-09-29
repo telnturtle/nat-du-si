5e06117ca788a58427d348781c28e3989f88cc32
Eom Hwi-yong <telnturtle@gmail.com>
2023-09-29 23:26:35 +0900

    feat. PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
         plugins: ['@emotion/babel-plugin'],
       },
     }),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
   build: {
     sourcemap: false,
